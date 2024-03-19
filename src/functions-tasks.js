636def30400087847b589a67dfa917a4b1746171
Katsiaryna Rubchanka <6923845@mail.ru>
2024-03-19 13:57:28 +0200

    feat: add solution "getPowerFunction"

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -77,8 +77,8 @@ function getArgumentsCount(funcs) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (item) => item ** exponent;
 }
 
 /**
