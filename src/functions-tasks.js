ff843edde72c504fd7269831b1e952a2226a87d1
Katsiaryna Rubchanka <6923845@mail.ru>
2024-03-19 14:31:48 +0200

    feat: add solution "memoize"

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -121,8 +121,14 @@ function getPolynom(a, b, c) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let res;
+  return () => {
+    if (!res) {
+      res = func();
+    }
+    return res;
+  };
 }
 
 /**
